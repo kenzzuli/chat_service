4a43e39cfa0cd238b4be4f05db739e7579f3b1f6
kenzzuli <820710063@qq.com>
2021-05-27 22:16:46 +0800

    删去冗余代码

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -3,23 +3,15 @@ import config
 from lib import cut
 from config import by_char
 import torch
-import os
 from chatbot.seq2seq import Seq2Seq
-
-# 使用gpu训练
-s2s = Seq2Seq().to(config.device)
-optimizer = torch.optim.Adam(s2s.parameters())
-
-# 模型加载
-if os.path.exists(config.chatbot_model_path) and os.path.exists(config.chatbot_optimizer_path):
-    s2s.load_state_dict(torch.load(config.chatbot_model_path, map_location=config.device))
-    optimizer.load_state_dict(torch.load(config.chatbot_optimizer_path, map_location=config.device))
+import random
 
 
 class Chatbot:
     def __init__(self):
         # 加载模型
-        self.s2s = s2s
+        self.s2s = Seq2Seq().to(config.device)
+        self.s2s.load_state_dict(torch.load(config.chatbot_model_path, map_location=config.device))
 
     def predict(self, sentence):
         self.s2s.eval()
@@ -31,6 +23,5 @@ class Chatbot:
         feature_length = torch.LongTensor([min(len(sentence), config.seq_len)]).to(config.device)
         # 预测
         y_predict = self.s2s.evaluate_beam_search(feature, feature_length)
-        # 转成句子
-        for i in y_predict:
-            print("".join(config.s2s_target.inverse_transform(i)))
+        # 任选一条转成句子并返回
+        return "".join(config.s2s_target.inverse_transform(random.choice(y_predict)))
