46637a15c3bc707c7b5df34c85a038542e8dd4cb
kenzzuli <820710063@qq.com>
2021-06-02 00:57:35 +0800

    minor bug

--- conf.d
+++ conf.d
@@ -1,6 +1,6 @@
 ;conf.d
 [program:chat_service]
-command=/Users/ken/PycharmProjects/nlp/bot/chat_service//run.sh  ;执行的命令
+command=/Users/ken/PycharmProjects/nlp/bot/chat_service/run.sh  ;执行的命令
 stdout_logfile=/Users/ken/PycharmProjects/nlp/bot/chat_service/log/out.log ;log的位置
 stderr_logfile=/Users/ken/PycharmProjects/nlp/bot/chat_service/log/error.log  ;错误log的位置
 directory=/Users/ken/PycharmProjects/nlp/bot/chat_service  ;路径
